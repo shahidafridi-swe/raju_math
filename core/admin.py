cac2da35b8c44f9559051898522a5fd7a2acac44
shahidafridi-swe <shahidafridi.cse@gmail.com>
2024-08-01 02:02:01 +0600

    update admin panel adminstration text

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import Banner, SuccessStudent
 
+admin.site.site_header = "Raju Math Administration"
+admin.site.site_title = "Raju Math Admin Portal"
+admin.site.index_title = "Welcome to Raju Math Administration"
+
 admin.site.register(Banner)
 admin.site.register(SuccessStudent)
